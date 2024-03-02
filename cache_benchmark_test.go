2eea56f4e76d88e58d811f11c1f06b22a16b7b2d
Eugene Simonov <es@madappgang.com>
2024-03-02 10:32:53 +0200

    Use new for range syntax in benchmark

--- cache_benchmark_test.go
+++ cache_benchmark_test.go
@@ -10,7 +10,7 @@ func Benchmark_LoadLocation(b *testing.B) {
 	for _, tc := range [2]string{"time", "locache"} {
 		for _, numLookups := range [3]int{10, 100, 1000} {
 			b.Run(fmt.Sprintf("%s_%d_RepeatedLookups", tc, numLookups), func(b *testing.B) {
-				for i := 0; i < b.N; i++ {
+				for range b.N {
 					b.StopTimer()
 
 					sut := time.LoadLocation
@@ -21,7 +21,7 @@ func Benchmark_LoadLocation(b *testing.B) {
 
 					b.StartTimer()
 
-					for j := 0; j < numLookups; j++ {
+					for range numLookups {
 						if _, err := sut("Europe/Kyiv"); err != nil {
 							b.Fatal(err)
 						}
